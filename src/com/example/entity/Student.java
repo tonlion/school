7e0cadce5c167eed4c8ae0d312bbff83c8c947f8
tonlion <1633537611@qq.com>
2015-08-31 22:53:25 +0800

    添加学生实体

---
+++ src/com/example/entity/Student.java
@@ -0,0 +1,210 @@
+package com.example.entity;
+
+public class Student {
+
+	private int cid;
+	private int ckState;
+	private String className;
+	private int isEnable;
+	private String majorName;
+	private int mid;
+	private String smsRight;
+	private String userHead;
+	private String userIdentity;
+	private int userLoginflag;
+	private String userMood;
+	private String userName;
+	private String userNickname;
+	private String userNo;
+	private String userPhonenumber;
+	private String userPwd;
+	private String userSchool;
+	private int userSign;
+	private int userTag;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int cid, int ckState, String className, int isEnable,
+			String majorName, int mid, String smsRight, String userHead,
+			String userIdentity, int userLoginflag, String userMood,
+			String userName, String userNickname, String userNo,
+			String userPhonenumber, String userPwd, String userSchool,
+			int userSign, int userTag) {
+		super();
+		this.cid = cid;
+		this.ckState = ckState;
+		this.className = className;
+		this.isEnable = isEnable;
+		this.majorName = majorName;
+		this.mid = mid;
+		this.smsRight = smsRight;
+		this.userHead = userHead;
+		this.userIdentity = userIdentity;
+		this.userLoginflag = userLoginflag;
+		this.userMood = userMood;
+		this.userName = userName;
+		this.userNickname = userNickname;
+		this.userNo = userNo;
+		this.userPhonenumber = userPhonenumber;
+		this.userPwd = userPwd;
+		this.userSchool = userSchool;
+		this.userSign = userSign;
+		this.userTag = userTag;
+	}
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public int getCkState() {
+		return ckState;
+	}
+
+	public void setCkState(int ckState) {
+		this.ckState = ckState;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public int getIsEnable() {
+		return isEnable;
+	}
+
+	public void setIsEnable(int isEnable) {
+		this.isEnable = isEnable;
+	}
+
+	public String getMajorName() {
+		return majorName;
+	}
+
+	public void setMajorName(String majorName) {
+		this.majorName = majorName;
+	}
+
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+
+	public String getSmsRight() {
+		return smsRight;
+	}
+
+	public void setSmsRight(String smsRight) {
+		this.smsRight = smsRight;
+	}
+
+	public String getUserHead() {
+		return userHead;
+	}
+
+	public void setUserHead(String userHead) {
+		this.userHead = userHead;
+	}
+
+	public String getUserIdentity() {
+		return userIdentity;
+	}
+
+	public void setUserIdentity(String userIdentity) {
+		this.userIdentity = userIdentity;
+	}
+
+	public int getUserLoginflag() {
+		return userLoginflag;
+	}
+
+	public void setUserLoginflag(int userLoginflag) {
+		this.userLoginflag = userLoginflag;
+	}
+
+	public String getUserMood() {
+		return userMood;
+	}
+
+	public void setUserMood(String userMood) {
+		this.userMood = userMood;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserNickname() {
+		return userNickname;
+	}
+
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
+
+	public String getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(String userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserPhonenumber() {
+		return userPhonenumber;
+	}
+
+	public void setUserPhonenumber(String userPhonenumber) {
+		this.userPhonenumber = userPhonenumber;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserSchool() {
+		return userSchool;
+	}
+
+	public void setUserSchool(String userSchool) {
+		this.userSchool = userSchool;
+	}
+
+	public int getUserSign() {
+		return userSign;
+	}
+
+	public void setUserSign(int userSign) {
+		this.userSign = userSign;
+	}
+
+	public int getUserTag() {
+		return userTag;
+	}
+
+	public void setUserTag(int userTag) {
+		this.userTag = userTag;
+	}
+
+}
